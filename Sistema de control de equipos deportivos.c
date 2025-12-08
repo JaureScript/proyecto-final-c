d767dd4e94c28302fa7dff04728c3f758f17584b
Rayan5611 <rayanherrera56@gmail.com>
2025-12-08 12:36:39 -0400

    [agregué mi nombre]RayanHerrera_28773197

--- Sistema de control de equipos deportivos.c
+++ Sistema de control de equipos deportivos.c
@@ -1,6 +1,7 @@
 /* INTEGRASTE:
-   -Juan Jaure / C.I: 31.302.572
-   -Blemncer carrion / C.I: 30.498.849
-   -Diego Benedetti / C.I: 30.956.790
-   -Deivys Paredes / C.I :30.956.510
+   -Juan Jaure / C.I:31.302.572
+   -Blemncer carrion / C.I:30.498.849
+   -Diego Benedetti / C.I:30.956.790
+   -Deivys Paredes / C.I:30.956.510
+   -Rayan Herrera /C.I:28.773.197
 */
