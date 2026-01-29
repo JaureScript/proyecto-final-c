f663504eba9301249b43945abdd8b8421b182408
Carrion <blemncercarrion@gmail.com>
2026-01-29 17:00:13 -0400

    Update Sistema de control de equipos deportivos.c
    Blemncer carrion / C.I:30.498.849
    añadiendo [librerias y estructura]

--- Sistema de control de equipos deportivos.c
+++ Sistema de control de equipos deportivos.c
@@ -5,3 +5,65 @@
    -Deivys Paredes / C.I:30.956.510
    -Rayan Herrera /C.I:28.773.197
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ARCHIVO "base_datos_equipos.dat"
+#define MAX_JUGADORES 50
+
+// 1. Definición de Estructuras
+typedef struct {
+    char nombre[50];
+    char posicion[30];
+    int dorsal;
+    int anotaciones;
+} Jugador;
+
+// Prototipos de Funciones
+// Esto le dice al compilador que las funciones existen más abajo.
+void limpiarPantalla();
+void limpiarBuffer();
+void pausar();
+
+//  Función Principal
+int main() {
+    Jugador lista[MAX_JUGADORES];
+    int n = 0;
+    int op;
+
+    do {
+        limpiarPantalla();
+        printf("=== CONTROL DE EQUIPOS ===\n");
+        printf("1. Registrar Jugador (Proximamente)\n");
+        printf("5. Salir\n");
+        printf("Seleccione: ");
+        
+        if (scanf("%d", &op) != 1) {
+            limpiarBuffer();
+            op = -1;
+        } else {
+            limpiarBuffer();
+        }
+    } while (op != 5);
+
+    return 0;
+}
+
+void limpiarPantalla() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+
+void limpiarBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void pausar() {
+    printf("\nPresione ENTER para continuar...");
+    getchar();
+}
